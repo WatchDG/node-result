cf2a0b6a05773d2848d9b20ea99b56eb9de7d0e7
WatchDG <baranov@w3m.co>
2020-07-20 11:33:48 +0300

    version 3.0.0

--- src/index.ts
+++ src/index.ts
@@ -2,41 +2,41 @@ type ErrorProcessing<Error, Data> = (error: Error) => Data;
 type ErrorProcessingAsync<Error, Data> = (error: Error) => Promise<Data>;
 
 export class Result<Error, Data> {
-    private readonly error: Error | null;
-    private readonly data: Data;
+  private readonly error: Error | null;
+  private readonly data: Data;
 
-    constructor(error: Error | null = null, data: Data) {
-        this.error = error;
-        this.data = data;
-    }
+  constructor(error: Error | null = null, data: Data) {
+    this.error = error;
+    this.data = data;
+  }
 
-    unwrap(): Data {
-        if (this.error !== null) {
-            throw this.error;
-        }
-        return this.data;
+  unwrap(): Data {
+    if (this.error !== null) {
+      throw this.error;
     }
+    return this.data;
+  }
 
-    unwrapAsync(): Promise<Data> {
-        if (this.error !== null) {
-            return Promise.reject(this.error);
-        }
-        return Promise.resolve(this.data);
+  unwrapAsync(): Promise<Data> {
+    if (this.error !== null) {
+      return Promise.reject(this.error);
     }
+    return Promise.resolve(this.data);
+  }
 
-    onError(func: ErrorProcessing<Error, Data>): Data {
-        if (this.error !== null) {
-            return func(this.error);
-        }
-        return this.data;
+  onError(func: ErrorProcessing<Error, Data>): Data {
+    if (this.error !== null) {
+      return func(this.error);
     }
+    return this.data;
+  }
 
-    async onErrorAsync(func: ErrorProcessingAsync<Error, Data>): Promise<Data> {
-        if (this.error !== null) {
-            return func(this.error);
-        }
-        return Promise.resolve(this.data);
+  async onErrorAsync(func: ErrorProcessingAsync<Error, Data>): Promise<Data> {
+    if (this.error !== null) {
+      return func(this.error);
     }
+    return Promise.resolve(this.data);
+  }
 }
 
 export const ResultOk = <Data>(data: Data) => new Result(null, data);
