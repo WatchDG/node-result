7afbb3d8dad5288548b7eff6bc63c9a422a1d6c5
WatchDG <baranov@w3m.co>
2020-07-20 11:33:09 +0300

    update

--- src/index.ts
+++ src/index.ts
@@ -2,40 +2,40 @@ type ErrorProcessing<Error, Data> = (error: Error) => Data;
 type ErrorProcessingAsync<Error, Data> = (error: Error) => Promise<Data>;
 
 export class Result<Error, Data> {
-    readonly #error: Error | null;
-    readonly #data: Data;
+    private readonly error: Error | null;
+    private readonly data: Data;
 
     constructor(error: Error | null = null, data: Data) {
-        this.#error = error;
-        this.#data = data;
+        this.error = error;
+        this.data = data;
     }
 
     unwrap(): Data {
-        if (this.#error !== null) {
-            throw this.#error;
+        if (this.error !== null) {
+            throw this.error;
         }
-        return this.#data;
+        return this.data;
     }
 
     unwrapAsync(): Promise<Data> {
-        if (this.#error !== null) {
-            return Promise.reject(this.#error);
+        if (this.error !== null) {
+            return Promise.reject(this.error);
         }
-        return Promise.resolve(this.#data);
+        return Promise.resolve(this.data);
     }
 
     onError(func: ErrorProcessing<Error, Data>): Data {
-        if (this.#error !== null) {
-            return func(this.#error);
+        if (this.error !== null) {
+            return func(this.error);
         }
-        return this.#data;
+        return this.data;
     }
 
     async onErrorAsync(func: ErrorProcessingAsync<Error, Data>): Promise<Data> {
-        if (this.#error !== null) {
-            return func(this.#error);
+        if (this.error !== null) {
+            return func(this.error);
         }
-        return Promise.resolve(this.#data);
+        return Promise.resolve(this.data);
     }
 }
 
