d3de687bf242af47b847b2cdf13fd525a9816f56
WatchDG <baranov@w3m.co>
2021-01-04 17:18:20 +0300

    add function tryCatchWrapper for decorator

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,6 @@ export { ResultError } from 'node-result-error';
 type ErrorProcessing<Error, Data> = (error: Error) => Data;
 type ErrorProcessingAsync<Error, Data> = (error: Error) => Promise<Data>;
 
-/**
- * Result
- */
 export class Result<Error, Data> {
   private readonly error: Error | null;
   private readonly data: Data;
@@ -52,18 +49,12 @@ export class Result<Error, Data> {
   }
 }
 
-/**
- * ResultOK
- */
 export class ResultOK<Data> extends Result<null, Data> {
   constructor(data: Data) {
     super(null, data);
   }
 }
 
-/**
- * ResultFAIL
- */
 export class ResultFAIL<Error> extends Result<Error, undefined> {
   constructor(error: Error) {
     super(error, void 0);
@@ -73,3 +64,31 @@ export class ResultFAIL<Error> extends Result<Error, undefined> {
 export const ResultOk = <Data>(data: Data) => new ResultOK(data);
 
 export const ResultFail = <Error>(error: Error) => new ResultFAIL(error);
+
+export function tryCatchWrapper(target: any, property: any, descriptor: { value: (...args: any[]) => any }) {
+  const self = descriptor.value;
+  descriptor.value = function (...args) {
+    try {
+      return self.call(this, ...args);
+    } catch (error) {
+      return ResultFail(error);
+    }
+  };
+  return descriptor;
+}
+
+export function tryCatchWrapperAsync(
+  target: any,
+  property: any,
+  descriptor: { value: (...args: any[]) => Promise<any> },
+) {
+  const self = descriptor.value;
+  descriptor.value = async function (...args) {
+    try {
+      return await self.call(this, ...args);
+    } catch (error) {
+      return ResultFail(error);
+    }
+  };
+  return descriptor;
+}
