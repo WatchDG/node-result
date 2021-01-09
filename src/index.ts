924449857ffafae334cd16b4f3324ef027d07f8c
WatchDG <baranov@w3m.co>
2021-01-10 02:05:38 +0300

    move unwrap methods to subclass

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,28 @@
 export { ResultError } from 'node-result-error';
 
-type ErrorProcessing<Error, Data> = (error: Error) => Data;
-type ErrorProcessingAsync<Error, Data> = (error: Error) => Promise<Data>;
+type ErrorProcessing<E, D> = (error: E) => D;
+type ErrorProcessingAsync<E, D> = (error: E) => Promise<D>;
 
 /**
  * class Result
  */
-export class Result<Error, Data> {
-  private readonly error: Error | null;
-  private readonly data: Data;
+export class Result<E, D> {
+  protected readonly error: E | null;
+  protected readonly data: D;
 
-  constructor(error: Error | null = null, data: Data) {
+  constructor(error: E | null = null, data: D) {
     this.error = error;
     this.data = data;
   }
 
-  unwrap(): Data {
-    if (this.error !== null) {
-      throw this.error;
-    }
-    return this.data;
-  }
-
-  unwrapAsync(): Promise<Data> {
-    if (this.error !== null) {
-      return Promise.reject(this.error);
-    }
-    return Promise.resolve(this.data);
-  }
-
-  onError(func: ErrorProcessing<Error, Data>): Data {
+  onError(func: ErrorProcessing<E, D>): D {
     if (this.error !== null) {
       return func(this.error);
     }
     return this.data;
   }
 
-  async onErrorAsync(func: ErrorProcessingAsync<Error, Data>): Promise<Data> {
+  async onErrorAsync(func: ErrorProcessingAsync<E, D>): Promise<D> {
     if (this.error !== null) {
       return func(this.error);
     }
@@ -55,18 +41,34 @@ export class Result<Error, Data> {
 /**
  * class ResultOK
  */
-export class ResultOK<Data> extends Result<null, Data> {
-  constructor(data: Data) {
+export class ResultOK<D> extends Result<null, D> {
+  constructor(data: D) {
     super(null, data);
+  }
+
+  unwrap(): D {
+    return this.data;
+  }
+
+  unwrapAsync(): Promise<D> {
+    return Promise.resolve(this.data);
   }
 }
 
 /**
  * class ResultFAIL
  */
-export class ResultFAIL<Error> extends Result<Error, undefined> {
-  constructor(error: Error) {
+export class ResultFAIL<E> extends Result<E, undefined> {
+  constructor(error: E) {
     super(error, void 0);
+  }
+
+  unwrap(): never {
+    throw this.error;
+  }
+
+  unwrapAsync(): Promise<E> {
+    return Promise.reject(this.error);
   }
 }
 
@@ -75,14 +77,14 @@ export class ResultFAIL<Error> extends Result<Error, undefined> {
  * @param data
  * @constructor
  */
-export const ResultOk = <Data>(data: Data): ResultOK<Data> => new ResultOK(data);
+export const ResultOk = <D>(data: D): ResultOK<D> => new ResultOK(data);
 
 /**
  * get a new instance of ResultFAIL
  * @param error
  * @constructor
  */
-export const ResultFail = <Error>(error: Error): ResultFAIL<Error> => new ResultFAIL(error);
+export const ResultFail = <E>(error: E): ResultFAIL<E> => new ResultFAIL(error);
 
 /**
  * `try catch` decorator for sync method
@@ -113,7 +115,7 @@ export function tryCatchWrapper(
  * @param descriptor
  */
 export function tryCatchWrapperAsync(
-  target: Record<any, any>,
+  target: { [Key: string]: unknown },
   property: string,
   descriptor: TypedPropertyDescriptor<(...args: any[]) => Promise<any>>
 ): TypedPropertyDescriptor<(...args: any[]) => Promise<ResultOK<any> | ResultFAIL<Error>>> {
